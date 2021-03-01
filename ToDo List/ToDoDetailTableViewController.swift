bed2065335572c70b64ac23fe231167319a5e7b2
Nipuni Obe <obe@gmail.com>
2021-02-28 23:34:23 -0500

    5.8 almost done

--- ToDo List/ToDoDetailTableViewController.swift
+++ ToDo List/ToDoDetailTableViewController.swift
@@ -12,22 +12,41 @@ class ToDoDetailTableViewController: UITableViewController {
     @IBOutlet weak var nameField: UITextField!
     @IBOutlet weak var datePicker: UIDatePicker!
     @IBOutlet weak var noteView: UITextView!
+    @IBOutlet weak var reminderSwitch: UISwitch!
+    @IBOutlet weak var dateLabel: UILabel!
     
     var toDoItem: ToDoItem!
+    
+    let datePickerIndexPath = IndexPath(row: 1, section: 1)
+    let notesTextIndexPath = IndexPath(row: 0, section: 2)
+    let notesRowHeight: CGFloat = 200
+    let defaultRowHeight: CGFloat = 44
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         if toDoItem == nil {
-            toDoItem = ToDoItem(name: "", date: Date( ), notes: "")
+            toDoItem = ToDoItem(name: "", date: Date( ), notes: "", reminderSet: false)
         }
+        updateUserInterface()
+//        if reminderSwitch.isOn {
+//            dateLabel.textColor = .black
+//        } else {
+//            dateLabel.textColor = .gray
+//
+//        }
+    }
+    
+    func updateUserInterface() {
         nameField.text = toDoItem.name
         datePicker.date = toDoItem.date
         noteView.text = toDoItem.notes
+        reminderSwitch.isOn = toDoItem.reminderSet
+        dateLabel.textColor = (reminderSwitch.isOn ? .black : .gray)
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        toDoItem = ToDoItem(name: nameField.text!, date: datePicker.date, notes: noteView.text)
+        toDoItem = ToDoItem(name: nameField.text!, date: datePicker.date, notes: noteView.text, reminderSet: reminderSwitch.isOn)
     }
     
     @IBAction func cancelButtonPressed(_ sender: UIBarButtonItem) {
@@ -36,6 +55,26 @@ class ToDoDetailTableViewController: UITableViewController {
             dismiss(animated: true, completion: nil)
         } else {
             navigationController?.popViewController(animated: true)
+        }
+    }
+    
+    
+    @IBAction func reminderSwitchChanged(_ sender: UISwitch) {
+        dateLabel.textColor = (reminderSwitch.isOn ? .black : .gray)
+        tableView.beginUpdates()
+        tableView.endUpdates()
+    }
+}
+
+extension ToDoDetailTableViewController {
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        switch indexPath {
+        case datePickerIndexPath:
+            return reminderSwitch.isOn ? datePicker.frame.height : 0
+        case notesTextIndexPath:
+            return notesRowHeight
+        default:
+            return defaultRowHeight
         }
     }
 }
