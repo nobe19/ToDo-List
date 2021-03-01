094106be7cf81b5e8d43a0c0f1ec51234c593fbb
Nipuni Obe <obe@gmail.com>
2021-02-28 20:55:18 -0500

    5.6 complete

---
+++ ToDo List/ToDoItem.swift
@@ -0,0 +1,14 @@
+//
+//  ToDoItem.swift
+//  ToDo List
+//
+//  Created by Nipuni Obe on 2/28/21.
+//
+
+import Foundation
+
+struct ToDoItem {
+    var name: String
+    var date: Date
+    var notes: String
+}
